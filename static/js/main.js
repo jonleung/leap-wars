4118272a5837016574d1686afb03762a75a93b78
Jonathan Leung <me@jonl.org>
2015-07-09 19:29:27 -0400

    add darth

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,18 @@
+var darth = document.getElementsByClassName('darth')[0];
+
 var options = { enableGestures: true };
 Leap.loop(options, function(frame) {  hand = frame.hands[0];
   if (hand === undefined) {
     sounds.breathing.stop();
     sounds.imperialMarch.stop();
+    darth.style.opacity = 0;
   }
   else {
     sounds.imperialMarch.ensurePlay();
     console.log(hand.grabStrength);
+        
+    darth.style.opacity = hand.grabStrength;
+
 
     if (hand.grabStrength > 0.5) {
       sounds.breathing.ensurePlay();
