febe20b34e12ac10076e3693dd7fc4d2e4fea8a0
Jonathan Leung <me@jonl.org>
2015-07-09 23:37:44 -0400

    started adding moving running aveage

--- static/js/audio.js
+++ static/js/audio.js
@@ -1,8 +1,24 @@
-var Sound = function(mp3Url) {
+var Sound = function(mp3Url, looping) {
   this.mp3Url = mp3Url;
   this.playing = false;
 
   this.audio = new Audio(mp3Url);
+
+  var sound = this;
+  
+  this.audio.addEventListener('ended', function() {
+    sound.playing = false;
+
+    // if (looping === true) {
+    //   if (mp3Url === 'http://soundfxcenter.com/movies/star-wars/b1a3c9_Lightsaber_Igniting_Sound_Effect.mp3') {
+    //     debugger
+    //   }
+    //   sound.audio.currentTime = 0;
+    //   sound.audio.play();  
+    // }    
+  }, false);
+
+
 };
 
 Sound.prototype.stop = function() {
@@ -19,8 +35,15 @@ Sound.prototype.ensurePlay = function() {
   }
 }
 
-var sounds = {};
-sounds.breathing = new Sound('http://f.cl.ly/items/2R45021y3b3X0v2S1C1s/Darth-vader-breathing.mp3');
-sounds.imperialMarch = new Sound('http://cprouvost.free.fr/fun/generiques/--%20Film%20--/Film%20-%20Star%20Wars%20(The%20Imperial%20March).mp3');
+Sound.prototype.play = function() {
+  this.stop();
+  this.audio.play();
+}
 
-debugger
+var sounds = {};
+sounds.breathing = new Sound('http://f.cl.ly/items/2R45021y3b3X0v2S1C1s/Darth-vader-breathing.mp3', true);
+sounds.choking = new Sound('http://f.cl.ly/items/3z1p2n3G0K0l311T1Y2W/choking.mp3', true);
+sounds.imperialMarch = new Sound('http://cprouvost.free.fr/fun/generiques/--%20Film%20--/Film%20-%20Star%20Wars%20(The%20Imperial%20March).mp3', true);
+sounds.turnOn = new Sound('http://soundfxcenter.com/movies/star-wars/b1a3c9_Lightsaber_Igniting_Sound_Effect.mp3');
+sounds.turnOff = new Sound('http://soundfxcenter.com/movies/star-wars/90f151_Lightsaber_Powering_Down_Sound_Effect.mp3');
+sounds.hum = new Sound('http://soundfxcenter.com/movies/star-wars/e2942b_Lightsaber_Idle_Hum_Sound_Effect.mp3', true);
--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,117 @@
+var round = function(x) {
+  return Math.round(x * 100) / 100
+}
+
+debug = false;
+
 var darth = document.getElementsByClassName('darth')[0];
+
+var lightSaberOn = false;
+
+var prevRoll = 0;
+var prevPitch = 0;
+var runningAverageFrames = 50;
+var frames = [];
+for(var i=0; i<runningAverageFrames; i++) {
+  frames.push(0);
+}
 
 var options = { enableGestures: true };
 Leap.loop(options, function(frame) {  hand = frame.hands[0];
+  
   if (hand === undefined) {
+    sounds.hum.stop();
     sounds.breathing.stop();
+    sounds.choking.stop();
     sounds.imperialMarch.stop();
     darth.style.opacity = 0;
   }
   else {
     sounds.imperialMarch.ensurePlay();
-    console.log(hand.grabStrength);
-        
-    darth.style.opacity = hand.grabStrength;
+
+    var lightSaberMode = hand.roll() < -0.7;
 
 
-    if (hand.grabStrength > 0.5) {
-      sounds.breathing.ensurePlay();
+    var roll = hand.roll();
+    var pitch = hand.pitch();
+
+    var delta = Math.sqrt(Math.pow(prevRoll - roll, 2) + Math.pow(prevPitch - pitch, 2));
+    if (delta === NaN) {
+      delta = 0;
     }
-    else {
+    frames.pop();
+    frames.push(delta);
+
+    var sum = 0;
+    frames.forEach(function(delta) {
+      sum += delta;
+    })
+    var movementRunningAverage = sum / runningAverageFrames;
+
+    var prevRoll = roll;
+    var prevPitch = pitch;
+
+    if (debug) {
+      debugger
+    }
+
+    console.log(
+      {
+        movementRunningAverage: movementRunningAverage,
+        // frameId: frame.id,
+        // lightSaberMode: lightSaberMode,
+        // lightSaberOn: lightSaberOn,
+        // grabStrength: round(hand.grabStrength),
+        roll: round(hand.roll()),
+        pitch: round(hand.pitch()),
+        yall: round(hand.yaw())
+      }
+    )
+
+
+
+    if (lightSaberMode) {
       sounds.breathing.stop();
+      sounds.choking.stop();
+      if (hand.grabStrength === 1) {
+        if (!lightSaberOn) {
+          $('.lightsaber').removeClass('hidden');
+          sounds.turnOn.ensurePlay();  
+          lightSaberOn = true; 
+        }
+        sounds.hum.ensurePlay();
+      }
+      else {
+        $('.lightsaber').addClass('hidden');
+        sounds.hum.stop();
+        if (lightSaberOn) {
+          lightSaberOn = false;
+          sounds.turnOff.ensurePlay();
+        }
+      }
     }
+    else { // force choke mode
+      darth.style.opacity = hand.grabStrength;
+
+      var chokingVolume = hand.grabStrength - 0.5;
+      if (chokingVolume < 0) {
+        chokingVolume = 0;
+      }
+      chokingVolume *= 2;
+      sounds.choking.audio.volume = chokingVolume;
+
+
+      if (hand.grabStrength > 0.5) {
+        sounds.breathing.ensurePlay();
+        sounds.choking.ensurePlay();
+      }
+      else {
+        sounds.breathing.stop();
+        sounds.choking.stop();
+      }
+    }
+
+        
   }
   
 });
